414e3940bbdadb6169a1fff10844002c5af74b39
Stephan Linz <linz@li-pro.net>
2024-01-17 18:01:39 +0100

    chore(sphinx): initial document project
    sphinx-quickstart \
      -p "Bridle tutorials" \
      -a "TiaC Systems" \
      -v "0.1.0" -r "0.1.0" -l de \
      --suffix .rst --master index \
      --ext-intersphinx \
      --ext-ifconfig \
      --ext-todo \
      --extensions sphinx_immaterial \
      --makefile --batchfile \
      --sep doc/cytron-maker-pi-rp2040
    
    - in: doc/cytron-maker-pi-rp2040/source/conf.py, change to:
    
      html_theme = 'sphinx_immaterial'
      rinoh_documents = [dict(doc='index', target='bridle-tutorials')]
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ doc/cytron-maker-pi-rp2040/source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Bridle tutorials'
+copyright = '2024, TiaC Systems'
+author = 'TiaC Systems'
+
+version = '0.1.0'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.todo',
+    'sphinx_immaterial',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'de'
+
+# -- Options for PDF output --------------------------------------------------
+# https://www.mos6581.org/rinohtype/master/quickstart.html#sphinx-quickstart
+
+rinoh_documents = [dict(doc='index', target='bridle-tutorials')]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_immaterial'
+html_static_path = ['_static']
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
